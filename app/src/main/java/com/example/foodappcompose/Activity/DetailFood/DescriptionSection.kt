7d07a930eb5bbf51ce4b22dd961e1f65927800ed
Hyder Ali <haiderbrohi222@gmail.com>
2025-08-02 17:21:08 +0500

    new things add like food details,description

---
+++ app/src/main/java/com/example/foodappcompose/Activity/DetailFood/DescriptionSection.kt
@@ -0,0 +1,40 @@
+package com.example.foodappcompose.Activity.DetailFood
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodappcompose.R
+
+@Composable
+fun DescriptionSection(description : String) {
+    Column {
+        Text(
+            text = "Details",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.black),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Text(
+            text = description,
+            fontSize = 16.sp,
+            color = colorResource(R.color.black),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Text(
+            text = "Buy 2 item for free delivery",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.black),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+    }
+}
--- app/src/main/java/com/example/foodappcompose/Activity/DetailFood/DetailEachFoodActivity.kt
+++ app/src/main/java/com/example/foodappcompose/Activity/DetailFood/DetailEachFoodActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +18,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.foodappcompose.Domain.FoodModel
 import com.example.foodappcompose.Helper.previewFood
@@ -35,10 +38,10 @@ class DetailEachFoodActivity : AppCompatActivity() {
         item.numberInCart = 1
         managmentCart = ManagmentCart(this)
 
-        setContent{
+        setContent {
             DetailScreen(
                 item = item,
-                onBackClick={finish() },
+                onBackClick = { finish() },
                 onAddToCartClick = {
                     managmentCart.insertItem(item)
                 }
@@ -59,15 +62,16 @@ fun DetailScreenPreview(modifier: Modifier = Modifier) {
 
 @Composable
 fun DetailScreen(item: FoodModel, onBackClick: () -> Unit, onAddToCartClick: () -> Unit) {
-   var numberInCart by remember{
-       mutableStateOf(item.numberInCart)
-   }
+    var numberInCart by remember {
+        mutableStateOf(item.numberInCart)
+    }
 
     ConstraintLayout {
         val (footer, column) = createRefs()
 
         Column(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
                 .background(colorResource(R.color.lightOrange))
                 .verticalScroll(rememberScrollState())
                 .constrainAs(column) {
@@ -78,7 +82,34 @@ fun DetailScreen(item: FoodModel, onBackClick: () -> Unit, onAddToCartClick: () 
                 .padding(bottom = 100.dp)
         ) {
 
-            HeaderSection(item = item,onBackClick = onBackClick)
+            HeaderSection(item = item, onBackClick = onBackClick)
+
+            TitleNumberRow(
+                item = item,
+                numberInCart = numberInCart,
+                onIncrement = {
+                    numberInCart++
+                    item.numberInCart = numberInCart
+                },
+                onDecrement = {
+                    if (numberInCart >1){
+                        numberInCart--
+                        item.numberInCart = numberInCart
+                    }
+                }
+            )
+
+            Text(
+                text = "$${item.Price}",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(R.color.black),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+
+            RowDetail(item)
+            DescriptionSection(item.Description)
         }
     }
 }
